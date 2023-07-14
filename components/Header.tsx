63df7ff380c736a2bf5b6fb6e122fdb613da8ee6
originlimit <andres-log@protonmail.com>
2023-07-14 08:06:42 -0700

    split middle of header into bar and icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,10 @@ const Header = () => {
     <header className='flex flex-col'>
       <div className="flex justify-between">
         <div className="flex">Logo</div>
-        <div className="flex">Search</div>
+        <div className="flex">
+          <div>Bar</div>
+          <div>Icon</div>
+        </div>
         <div className="flex">Acc</div>
       </div>
       <div className='flex justify-center'>Filters</div>
