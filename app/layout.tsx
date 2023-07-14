509ef6d0960a4ae6d1c867810339d03558f9dd51
originlimit <andres-log@protonmail.com>
2023-07-14 08:04:33 -0700

    added the footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({
         <main className='flex flex-col bg-red-300'>
           <Header />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
